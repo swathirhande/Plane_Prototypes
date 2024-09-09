10b176da3ad62f5e6074ff5212b2c347b5308841
swathirhande <swathi@usc.edu>
2024-09-09 14:50:08 -0700

    Initial changes to the project

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f; 
+    public float verticalInput;
+
+    
+    void Start()
+    {
+
+    }
+
+    
+    void FixedUpdate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
+        
+    }
+}
+
+
